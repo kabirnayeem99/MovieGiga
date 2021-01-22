3087561d92b43a311535ba3907aa34fb96bd500d
Naimul Kabir <kabirnayeem.99@gmail.com>
2021-01-22 16:55:32 +0600

    👌 improve: adds movie details layout

---
+++ app/src/main/java/io/kabirnayeem99/moviegiga/movie_details/MovieDetails.kt
@@ -0,0 +1,12 @@
+package io.kabirnayeem99.moviegiga.movie_details
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.kabirnayeem99.moviegiga.R
+
+class MovieDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
